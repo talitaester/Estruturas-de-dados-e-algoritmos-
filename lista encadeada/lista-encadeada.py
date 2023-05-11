c9b94a819e6f92668bb5fa10d945f5ce6c45d245
Talita Ester <talita.alves@ufba.br>
2023-05-11 20:48:32 +0000

    renamed:    lista-encadeada.py -> lista encadeada/lista-encadeada.py 	renamed:    primeira-aula.py -> "lista est\303\241tica/lista est\303\241tica.py" 	deleted:    lista_enc.py 	renamed:    lista_1.py -> run codes/lista_1.py 	renamed:    testantado lista 1.py -> run codes/testantado lista 1.py

--- lista encadeada/lista-encadeada.py
+++ lista encadeada/lista-encadeada.py
@@ -6,10 +6,10 @@ class Element:
         self.prox = None
 
     def showNode(self):
-        return (f'{self.value}')
+        return (self.value)
 
 class ListEnc:
-    def __init__(self):
+    def __init__(self): 
         self.head = None
         self.size = 0
     
@@ -18,7 +18,7 @@ class ListEnc:
             temp = self.head
             print('**LISTA**')
             while temp is not None:
-                print(f'{temp.showNode()}')
+                print(temp.showNode())
                 temp = temp.prox
 
 
@@ -37,17 +37,30 @@ class ListEnc:
         node = Element(no)
         if self.head is not None:
             current_node = self.head
-            print('im here')
             while True:               
                 if current_node.prox is not None:
                     current_node = current_node.prox
+        
                 else:
                     current_node.prox = node
                     self.size += 1
                     break
         else:
             self.head = node
-    
+
+    def insertInPosition(self, no, pos):
+        
+        node = Element(no)
+        temp = self.head
+        past = None
+        for i in range(pos):
+            past = temp
+            temp = temp.prox
+            if i == pos-1:
+                print("im here")
+                past.prox = node
+                node.prox = temp
+
 
 def main():
     testList = ListEnc()
@@ -55,7 +68,10 @@ def main():
     testList.insert(2)
     testList.insert(3)
     testList.endInsert(8)
+    testList.insertInPosition(7, 2)
     testList.show()
 
 if __name__ == '__main__':
     main()
+
+
