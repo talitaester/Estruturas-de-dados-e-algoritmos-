3d50eb1b2db06f21bf2d3ef9191ed4c38a9c71a2
Talita Ester <talita.alves@ufba.br>
2023-05-15 14:54:32 +0000

    modified:   "lista est\303\241tica/lista est\303\241tica.py"

--- lista estática/lista estática.py
+++ lista estática/lista estática.py
@@ -1,31 +1,62 @@
+
+
 class listES:
     def __init__(self, max):
+        self.head = None
         self.size = 0
         self.max = max
         self.objList = [None] * max
 
 
-    def insert(self, obj):
-        if self.size < self.max:
-          self.objList[self.size] = obj
-          self.size += 1
+    def insert(self, no):
+        if self.size == 0:
+           self.objList[0] = no
+           self.head = no
+           self.size += 1
+        elif self.size < self.max:
+           self.objList[self.size] = no
+           self.size += 1
+                 
+                 
 
-    def __str__(self):
-      return str(self.objList[:self.size])
+    def insertX(self, position, value):
+       obj = value
+       iter = self.size - position
+       if self.size < self.max:
+          for i in range(self.size, position -1, 1):
+            self.objList[i] = self.objList[i+1]
+          self.objList[position +1] = obj
+
+             
+
+
+
+    def showList(self):
+      for i in self.objList:
+        print(i)
+    
      
 def main():
-  end = 0
+ #end = 0
   list_size = int(input("digite o tamanho da lista "))
   testList = listES(list_size)
-  while end == 0:
-    m =  int(input("digite um valor para inserir "))
-    if testList.size >=list_size:
-       print("sua lista atingiu o tamanho maximo :(")
-    else:
-       testList.insert(m)
-       print(testList)
-    end = int(input("0 para continuar e 1 para encerrar "))
-  print(testList)
+ #while end == 0:
+ #  m =  int(input("digite um valor para inserir "))
+ #  if testList.size >=list_size:
+ #     print("sua lista atingiu o tamanho maximo :(")
+ #  else:
+ #     testList.insert(m)
+ #     print(testList)
+ #  end = int(input("0 para continuar e 1 para encerrar "))
+  testList.insert(1)
+  testList.insert(2)
+  testList.insert(3)
+  testList.insert(4)
+  testList.insert(5)
+  testList.insert(6)
+  testList.insertX(3, 9)
+  testList.showList()
+
 
 
 if __name__ == '__main__':
