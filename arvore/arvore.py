a7593d56b0c0316bb4216c797c215a2cf7f1f280
Talita Ester <talita.alves@ufba.br>
2023-05-25 21:01:14 +0000

    new file:   arvore/arvore.py 	deleted:    pilha&fila.txt

---
+++ arvore/arvore.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+    
+    def showNode(self):
+        return self.value
+
+class Tree:
+    def __init__(self):
+        self.root = None
+        self.height = 0
+
+    def insert
+    
